697417392b8b7e56ca71d424d9ffab11b98bec06
samuelpowellweb <samuelpowellweb@gmail.com>
2026-09-25 11:02:07 +0100

    working with strings

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 const darkModeToggle = document.querySelector('.dark-mode-toggle');
 const body = document.body;
+let theme = 'light';
 
 darkModeToggle.addEventListener('click', function () {
-    body.classList.toggle('dark');
-    // if dark mode is on, change text to 'Light Mode'
-    if (body.classList.contains('dark')) {
+    if (theme === 'light') {
+        body.classList.add('dark');
+        darkModeToggle.classList.add('active');
         darkModeToggle.textContent = 'Light Mode';
-    } else darkModeToggle.textContent = 'Dark Mode';
+        theme = 'dark';
+        console.log(darkMode);
+    } else if (theme === 'dark') {
+        body.classList.remove('dark');
+        darkModeToggle.classList.remove('active');
+        darkModeToggle.textContent = 'Dark Mode';
+        theme = 'light';
+        console.log(darkMode);
+    }
 })
+
+// get local storage. If null, set them to light
